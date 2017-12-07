82769d35b080364e20af8d6407fb8c7f7eb626a8
Serdyuk <serduksergey90@gmail.com>
2017-12-07 18:24:12 +0200

    add layout for stopwatch. 3 buttons, textview

--- app/src/main/java/com/serdyuk/stopwatch/StopwatchActivity.java
+++ app/src/main/java/com/serdyuk/stopwatch/StopwatchActivity.java
@@ -2,6 +2,7 @@ package com.serdyuk.stopwatch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class StopwatchActivity extends AppCompatActivity {
 
@@ -9,5 +10,17 @@ public class StopwatchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stopwatch);
+    }
+
+    public void onClickStart(View view) {
+        System.out.println("Clicked!");
+    }
+
+    public void onClickStop(View view) {
+        System.out.println("Clicked!");
+    }
+
+    public void onClickReset(View view) {
+        System.out.println("Clicked!");
     }
 }
