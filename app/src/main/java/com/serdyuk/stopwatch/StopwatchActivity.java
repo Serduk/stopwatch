0d0b522450a4985c0f20eff5b45543f1f90080c8
Serdyuk <serduksergey90@gmail.com>
2017-12-18 00:33:22 +0200

    Add working states for stopwatch. \n Add Bundle for changing orientation on device

--- app/src/main/java/com/serdyuk/stopwatch/StopwatchActivity.java
+++ app/src/main/java/com/serdyuk/stopwatch/StopwatchActivity.java
@@ -1,46 +1,77 @@
 package com.serdyuk.stopwatch;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.TextView;
 
 public class StopwatchActivity extends Activity {
     private int seconds = 0;
-    private boolean isRuning = false;
+    private boolean isRunning = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stopwatch);
+        /*
+         * Check conditional.
+          * If bundle something have -> add this to param
+         * */
+        if (savedInstanceState != null) {
+            seconds = savedInstanceState.getInt("seconds");
+            isRunning = savedInstanceState.getBoolean("isRunning");
+        }
+        runTimer();
+    }
+
+    /**
+     * Method for saving app states
+     * And then, you can send this states in any method. for example: onCreate()
+     * */
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putInt("seconds", seconds);
+        savedInstanceState.putBoolean("isRunning", isRunning);
+
     }
 
     public void onClickStart(View view) {
-        isRuning = true;
+        isRunning = true;
     }
 
     public void onClickStop(View view) {
-        isRuning = false;
+        isRunning = false;
     }
 
     public void onClickReset(View view) {
-        isRuning = false;
+        isRunning = false;
         seconds = 0;
     }
 
     private void runTimer() {
         final TextView timeView = (TextView) findViewById(R.id.time_view);
+        final Handler handler = new Handler();
 
-        int hours = seconds/3600;
-        int minutes = (seconds%3600)/60;
-        int secs = seconds%60;
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds/3600;
+                int minutes = (seconds%3600)/60;
+                int secs = seconds%60;
 
-        String time = String.format("%d:%02d:%02d", hours, minutes, secs);
+                @SuppressLint("DefaultLocale")
+                String time = String.format("%d:%02d:%02d", hours, minutes, secs);
 
-        timeView.setText(time);
+                timeView.setText(time);
 
-        if (isRuning) {
-            seconds++;
-        }
+                if (isRunning) {
+                    seconds++;
+                }
+
+                handler.postDelayed(this, 1000);
+            }
+        });
     }
 }
